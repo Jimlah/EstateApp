cf26d5d7672fd663313ddd3580a91ddc096db06c
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-11-30 14:51:41 +0100

    feat: Admin model created

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
