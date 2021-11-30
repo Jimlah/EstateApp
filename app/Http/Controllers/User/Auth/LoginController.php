ac986d6c37290174709f82e9bef7af2f08642d7d
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-11-30 15:43:07 +0100

    Created user Login

--- app/Http/Controllers/User/Auth/LoginController.php
+++ app/Http/Controllers/User/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
+use App\Models\Admin;
 
 class LoginController extends Controller
 {
@@ -13,12 +14,18 @@ class LoginController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (!auth()->guard('user')->attempt($credentials)) {
-            return redirect()->route('user.dashboard');
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
         }
+
+        $user = Admin::find(auth()->guard('user')->id);
 
         return response()->json([
             'message' => 'Login Successful',
             'status' => 'success',
+            'user' => $user,
+            'token' => $user->createToken('user')->accessToken,
             'redirect' => route('user.dashboard')
         ]);
     }
--- routes/api/user.php
+++ routes/api/user.php
@@ -1,4 +1,7 @@
 <?php
 
+use App\Http\Controllers\User\Auth\LoginController;
 
-Route::group(['prefix' => 'user', 'middleware' => ['auth:user-api', 'scopes:user']], function () {});
+Route::group(['prefix' => 'user', 'middleware' => ['auth:user-api', 'scopes:user']], function () {
+    Route::get('login', LoginController::class);
+});
