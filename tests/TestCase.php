f40f44852325e86f39369746582c7d662313dec4
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-12-04 10:47:47 +0100

    feat: added documentation for api

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Styde\Enlighten\Tests\EnlightenSetup;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -9,10 +10,12 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use RefreshDatabase;
+    use EnlightenSetup;
 
     protected function setUp(): void
     {
         parent::setUp();
+        $this->setUpEnlighten();
         $this->artisan('passport:install');
     }
 }
