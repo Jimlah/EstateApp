50198f9c94667b1f80a8e474e0b1071876a9dc2e
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-12-20 07:27:47 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment(['local', 'testing'])) {
+        // if ($this->app->environment(['local', 'testing'])) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
-            $this->app->register( Styde\Enlighten\Providers\EnlightenServiceProvider::class);
-        }
+            $this->app->register(Styde\Enlighten\Providers\EnlightenServiceProvider::class);
+        // }
     }
 
     /**
