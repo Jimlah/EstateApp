8f196d34f6f18c29f11dde4bcb67d436019fd295
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-12-20 06:18:22 +0100

    Added custom users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Admin;
+use App\Models\Manager;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Artisan::call('passport:install');
         Admin::factory(5)->create();
 
         $this->call([
@@ -27,6 +31,38 @@ class DatabaseSeeder extends Seeder
         Admin::first()->update([
             'email' => 'superadmin@admin.com',
             'password' => 'password',
+            'is_admin' => true,
+        ]);
+
+        Admin::find(2)->update([
+            'email' => 'admin@admin.com',
+            'password' => 'password',
+            'is_admin' => false,
+        ]);
+
+
+        Manager::first()->update([
+            'email' => 'superadmin@manager.com',
+            'password' => 'password',
+            'is_admin' => true,
+        ]);
+
+        Manager::find(2)->update([
+            'email' => 'admin@manager.com',
+            'password' => 'password',
+            'is_admin' => false,
+        ]);
+
+        User::first()->update([
+            'email' => 'admin@house.com',
+            'password' => 'password',
+            'is_admin' => true,
+        ]);
+
+        User::find(2)->update([
+            'email' => 'user@house.com',
+            'password' => 'password',
+            'is_admin' => false,
         ]);
     }
 }
