ba73d3f49e436c1af7fe34ade57a94c08756fbd1
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2022-01-02 20:21:14 +0100

    Update House resource

--- app/Http/Resources/HouseResource.php
+++ app/Http/Resources/HouseResource.php
@@ -14,6 +14,12 @@ class HouseResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'estate' => new EstateResource($this->estate),
+            'category' => $this->category,
+            'code' => $this->code,
+            'created_at' => $this->created_at->format('Y-m-d'),
+        ];
     }
 }
